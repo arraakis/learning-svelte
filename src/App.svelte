83abde15cdc3976a12c99ee3bfcc8825dedbfe24
leythadan <16468186+leythadan@users.noreply.github.com>
2022-11-05 00:36:16 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import PlayerScore from './PlayerScore.svelte'
+
+  let player1Component;
+  let player2Component;
+
+  function reset() {
+    player1Component.reset();
+    player2Component.reset();
+  }
+</script>
+
+<main>
+  <PlayerScore bind:this={player1Component} playername="Player 1"/>
+  <PlayerScore bind:this={player2Component} playername="Player 2"/>
+  <button on:click={reset} class="contrast outline">RESET</button>
+</main>
+
+<style>
+ 
+</style>
---
+++ src/PlayerScore.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let playername;
+    let playerScore = 0;
+
+    function increasePlayerScore() {
+        playerScore += 1;
+    }
+
+    function decreasePlayerScore() {
+        playerScore -= 1;
+        if (playerScore < 0) {
+            playerScore = 0;
+        }
+    }
+
+    export function reset() {
+        playerScore = 0;
+    }
+
+
+
+
+
+</script>
+
+<div>
+    <h2 class="center">{playername}</h2>
+    <h3 class="center">{playerScore}</h3>
+    <button on:click={increasePlayerScore}>Increase</button>
+    <button on:click={decreasePlayerScore}>Decrease</button>
+</div>
+
+<style>
+    .center {
+        text-align: center;
+    }
+</style>
